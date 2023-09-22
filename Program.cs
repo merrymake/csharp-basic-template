dc9f5bc39845ddd857f47229e554b0590485a03a
Christian Clausen <maxclausen@gmail.com>
2023-09-22 10:34:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using eu.merrymake.service.csharp;
+
+namespace template.basic
+{
+    internal class Program
+    {
+
+        static void HandleHello(byte[] payloadBytes, Envelope envelope)
+        {
+            string payload = System.Text.Encoding.UTF8.GetString(payloadBytes);
+            Merrymake.ReplyToOrigin($"Hello, {payload}!", MimeType.txt);
+        }
+
+        static void Main(string[] args)
+        {
+            Merrymake.Service(args)
+                     .Handle("handleHello", HandleHello);
+        }
+
+    }
+}
